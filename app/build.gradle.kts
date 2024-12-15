801856f9e3119b1af7ab1652ae356bdeef0a126c
junaid-131 <jk2673114@gmail.com>
2024-12-16 00:09:48 +0500

    Your commit message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
+    implementation (libs.imageslideshow)
 }
